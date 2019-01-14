ca572e6a8366c955fcad46ad118bf2076b1ff6fc
Leonardo Souza <sarival@sarival.com.br>
2019-01-14 13:47:00 +0000

    Refatorando animação do botão do menu com JQuery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-let botao = document.querySelector(".menu-principal__btn");
-let menuPrincipal = document.querySelector(".menu-principal")
+// let botao = document.querySelector(".menu-principal__btn");
+// let menuPrincipal = document.querySelector(".menu-principal")
 
-botao.addEventListener("click", abrirFecharMenu);
+// botao.addEventListener("click", abrirFecharMenu);
 
-function abrirFecharMenu(){
-    menuPrincipal.classList.toggle("menu-principal--fechado");
-}
+// function abrirFecharMenu(){
+//     menuPrincipal.classList.toggle("menu-principal--fechado");
+// }
+
+//fazendo com jquery
+$(".menu-principal__btn").click(function(){
+    $(".menu-principal").toggleClass("menu-principal--fechado");
+});
