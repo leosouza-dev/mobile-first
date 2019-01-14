dc8679c04250b70cb1bc35757ed107d73164f745
Leonardo Souza <sarival@sarival.com.br>
2019-01-14 16:21:46 +0000

    Finalizado o slide das imagens de depoimento usando plugin jaquery - slick

--- js/main.js
+++ js/main.js
@@ -7,7 +7,19 @@
 //     menuPrincipal.classList.toggle("menu-principal--fechado");
 // }
 
-//fazendo com jquery
-$(".menu-principal__btn").click(function(){
-    $(".menu-principal").toggleClass("menu-principal--fechado");
+
+$(document).ready(function(){
+    
+    //fazendo botão menu-principal com jquery
+    $(".menu-principal__btn").click(function(){
+        $(".menu-principal").toggleClass("menu-principal--fechado");
+    });
+    
+    //plugin slick - carrousel/slide de imagens - depoimentos
+    $(".depoimentos__caixa").slick({
+        autoplay: true,
+        arrows: false,
+        dots: true
+    });
 });
+
